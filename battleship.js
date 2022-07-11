73dafb760ee9bf781a346ce92120f1115a1f6444
Ryan <rygho90@gmail.com>
2022-07-11 18:10:52 -0400

    Revise shipFactory function to take coordinate array as parameter

--- battleship.js
+++ battleship.js
@@ -1,9 +1,11 @@
-const shipFactory = (shipLength) => {
+const shipFactory = (coordinateArr) => {
+    const shipLength = coordinateArr.length
     const beenHit = [];
-
     let sunk = false;
 
-    const hit = pos => beenHit.push(pos)
+    const hit = (coordinate) => {
+        if (coordinateArr.includes(coordinate)) beenHit.push(coordinate)
+    }
 
     const isSunk = (beenHit) => {
         sunk = (beenHit.length === shipLength) ? true : false;
