cf223b53b7f0cb7613bcd13c97c2ace44702a669
Ryan <rygho90@gmail.com>
2022-07-12 20:52:16 -0400

    Add ability to place ship horizontally with placeShip function

--- battleship.js
+++ battleship.js
@@ -1,25 +1,49 @@
 const shipFactory = (coordinateArr) => {
     const shipLength = coordinateArr.length
-    const beenHit = [];
+    const beenHit = []
+    const shipLocation = coordinateArr
 
     const hit = (coordinate) => {
         if (coordinateArr.includes(coordinate)) beenHit.push(coordinate)
     }
 
     const isSunk = () => {
-        return (beenHit.length === shipLength) ? true : false;
-    } 
+        return (beenHit.length === shipLength) ? true : false
+    }
 
-    return {shipLength, beenHit, hit, isSunk}
+    return {shipLength, beenHit, shipLocation, hit, isSunk}
 }
 
 const gameboardFactory = () => {
+    const allCoords = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', // 0-9
+                       'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', // 10-19
+                       'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10',
+                       'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10',
+                       'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10',
+                       'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10',
+                       'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9', 'G10',
+                       'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10',
+                       'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10',
+                       'J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'J9', 'J10']
 
-    const placeShip = (direction, shipLength, startingCoordinate) => {
+    const placeShip = (direction, shipLength, startPos) => {
         if (direction === 'horizontal') {
-            // ...
+            let row = Math.floor(startPos / 10)
+            let endPos = startPos + shipLength
+            const coordinateArr = []
+
+            if (row === Math.floor(endPos / 10)) {
+                for (let i = 0; i < shipLength; i++) {
+                    coordinateArr.push(allCoords[i + startPos])
+                }
+                return shipFactory(coordinateArr)
+            } else {
+                return null
+            }
         }
     }
+
+    return {placeShip}
 }
 
 export { shipFactory, gameboardFactory }
--- battleship.test.js
+++ battleship.test.js
@@ -2,8 +2,6 @@ import {shipFactory, gameboardFactory} from './battleship'
 
 test('create a ship object', () => {
     const newShip = shipFactory(['A1', 'A2'])
-    expect(newShip).toHaveProperty('shipLength', 2)
-    expect(newShip).toHaveProperty('beenHit', [])
     expect(newShip).toHaveProperty('hit')
     expect(newShip).toHaveProperty('isSunk')
 })
@@ -20,4 +18,19 @@ test("test the ship's isSunk method", () => {
     newShip.hit('A1')
     newShip.hit('A2')
     expect(newShip.isSunk()).toBe(true)
+})
+
+test("test placing a ship horizontally", () => {
+    const gameBoard = gameboardFactory()
+    const shipOne = gameBoard.placeShip('horizontal', 3, 2)
+    expect(shipOne).toHaveProperty('shipLength', 3)
+    expect(shipOne).toHaveProperty('shipLocation', ['A3', 'A4', 'A5'])
+
+    const shipTwo = gameBoard.placeShip('horizontal', 4, 5)
+    expect(shipTwo).toHaveProperty('shipLength', 4)
+    expect(shipTwo).toHaveProperty('shipLocation', ['A6', 'A7', 'A8', 'A9'])
+
+    // Ship too long for row, returns null
+    const shipThree = gameBoard.placeShip('horizontal', 5, 8)
+    expect(shipThree).toBe(null)
 })
