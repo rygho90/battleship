80d85a48041154c2d343aaaf72bb06a8854a5986
Ryan <rygho90@gmail.com>
2022-07-16 13:37:28 -0400

    Complete basic UI in HTML/CSS

--- src/battleship.js
+++ src/battleship.js
@@ -3,10 +3,18 @@ import gameboard from './gameboard'
 import player from './player'
 import './styles.css'
 
-const computerArea = document.querySelector('[data-computer-area]')
-const humanArea = document.querySelector('[data-human-area]')
+const computerGrid = document.querySelector('[data-computer-grid]')
+const humanGrid = document.querySelector('[data-human-grid]')
 
 const computerBoard = gameboard();
 const humanBoard = gameboard();
 
+function createBoard(grid) {
+    for (let i = 0; i < 100; i++) {
+        const square = document.createElement('div')
+        grid.append(square)
+    }
+}
 
+createBoard(computerGrid)
+createBoard(humanGrid)
